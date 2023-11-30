af3b501220a87cdb8ca5ef8301a5b70013d6c9d6
harshVrdn03 <112956544+harshVrdn03@users.noreply.github.com>
2023-11-30 19:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, jsonify
 from picpurify.api import PicPurifyImage, PicPurifyException
 import os
+import requests
+import json
 
+app = Flask(__name__)
+
+# Replace these placeholder values with your actual API keys and workflow
 PICPURIFY_API_KEY = os.environ.get('PICPURIFY_API_KEY')
 PICPURIFY_MODERATION_CLASSES = ['porn_moderation', 'weapon_moderation']
 
+SIGHTENGINE_API_USER = os.environ.get('SIGHTENGINE_API_USER')
+SIGHTENGINE_API_SECRET = os.environ.get('SIGHTENGINE_API_SECRET')
+SIGHTENGINE_WORKFLOW = os.environ.get('SIGHTENGINE_WORKFLOW')
+
 # Initialize PicPurifyImage client
 image_client = PicPurifyImage(PICPURIFY_API_KEY, PICPURIFY_MODERATION_CLASSES)
-app = Flask(__name__)
+
 @app.route('/check_moderation', methods=['POST'])
 def check_moderation():
     try:
@@ -29,4 +38,34 @@ def check_moderation():
 
     except PicPurifyException as e:
         return jsonify({'error': str(e)}), 500
-        
+
+
+@app.route('/video_moderation', methods=['POST'])
+def video_moderation():
+    try:
+        # Get the video URL from the request
+        video_url = request.json.get('video_url')
+
+        # Check if the 'video_url' parameter is present in the request
+        if not video_url:
+            return jsonify({'error': 'Video URL is required'}), 400
+
+        # Set up parameters for Sightengine API
+        sightengine_params = {
+            'stream_url': video_url,
+            'workflow': SIGHTENGINE_WORKFLOW,
+            'api_user': SIGHTENGINE_API_USER,
+            'api_secret': SIGHTENGINE_API_SECRET
+        }
+
+        # Make a request to Sightengine API
+        sightengine_response = requests.get('https://api.sightengine.com/1.0/video/check-workflow-sync.json', params=sightengine_params)
+
+        # Parse the Sightengine API response
+        sightengine_output = json.loads(sightengine_response.text)
+
+        # Video accepted
+        return jsonify({'status': 'Video Accepted', 'details': sightengine_output}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
